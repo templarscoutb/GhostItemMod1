5c8913241b6a926929322c63667c3d6fc5415e04
Paymon the Psyker <56231316+templarscoutb@users.noreply.github.com>
2020-09-05 16:33:19 -0400

    Working

--- GhostItemMod1/GhostItem.cs
+++ GhostItemMod1/GhostItem.cs
@@ -8,11 +8,10 @@ namespace GhostItemMod1
 {
     public class GhostItem
     {
-        
+
         public static ItemIndex GhostIndex;
-        public static GameObject GhostPrefab;
-        
-        
+
+
 
         public static void Init()
         {
@@ -20,13 +19,14 @@ namespace GhostItemMod1
             AddTokens();
             AddItem();
             On.RoR2.CharacterBody.OnInventoryChanged += orig_CharacterBody_OnInventoryChanged;
-            On.RoR2.ItemFollower.Update += orig_ItemFollow_Update;
+
         }
 
         private static void AddProvider()
         {
-                                                                                                         //namespace.asset bundle name
-            using (System.IO.Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("GhostItemMod1.ghostmod"))
+            //namespace.asset bundle name
+            using (System.IO.Stream stream =
+                Assembly.GetExecutingAssembly().GetManifestResourceStream("GhostItemMod1.ghostmod"))
             {
                 AssetBundle bundle = AssetBundle.LoadFromStream(stream);
                 AssetBundleResourcesProvider provider = new AssetBundleResourcesProvider("@GhostBundle", bundle);
@@ -42,16 +42,10 @@ namespace GhostItemMod1
             string longLore = "TBW";
             LanguageAPI.Add("GHOST_LORE_TOKEN", longLore);
         }
-        public static void orig_ItemFollow_Update(On.RoR2.ItemFollower.orig_Update orig, ItemFollower self)
-        {
-            GhostPrefab = self.followerPrefab;
-            orig(self);
-        }
+
 
         private static void AddItem()
-        { 
-        
-                
+        {
             ItemDef def = new ItemDef
             {
                 name = "GhostItem",
@@ -62,33 +56,141 @@ namespace GhostItemMod1
                 tier = ItemTier.Tier1,
                 pickupIconPath = "@GhostBundle:Assets/Ghost/GhostIcon.png",
                 pickupModelPath = "@GhostBundle:Assets/Ghost/Ghost.prefab",
-                canRemove = true,
+                canRemove = false,
                 hidden = false,
                 tags = new ItemTag[]
                 {
                     ItemTag.Utility,
-                    ItemTag.OnKillEffect
+                    ItemTag.Healing
                 }
             };
 
-            
-            GameObject GhostPrefab = Resources.Load<GameObject>("@GhostBundle:Assets/Ghost/Ghost.prefab");
-            
-            ItemDisplayRuleDict rules = new ItemDisplayRuleDict(new ItemDisplayRule[]
+            GameObject ghostPrefab = Resources.Load<GameObject>("@GhostBundle:Assets/Ghost/Ghost.prefab");
+            Vector3 generalScale = new Vector3(1.0f, 1.0f, 1.0f);
+           ItemDisplayRuleDict rules = new ItemDisplayRuleDict(new ItemDisplayRule[]
             {
                 new ItemDisplayRule
                 {
                     ruleType = ItemDisplayRuleType.ParentedPrefab,
-                    followerPrefab = GhostPrefab,
-                    childName = "chest",
+                    followerPrefab = ghostPrefab,
+                    childName = "Pelvis",
                     localPos = new Vector3(-0.22f, 0f, 0f),
-                    localAngles = new Vector3(0f, 0f, 0f),
-                    localScale = new Vector3(0f,0f,0f)
+                    localAngles = new Vector3(0f, -0.05f, 0f),
+                    localScale = generalScale
                 }
-            }); 
-            
-            CustomItem item = new CustomItem(def, rules);
-            GhostIndex = ItemAPI.Add(item);
+            });
+            rules.Add("mdlHuntress", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ghostPrefab,
+                    childName = "Chest",
+                    localPos = new Vector3(0f, 0.75f, -0.05f),
+                    localAngles = new Vector3(0f, 0f, 0f),
+                    localScale = generalScale
+                }
+            });
+            rules.Add("mdlToolbot", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ghostPrefab,
+                    childName = "LowerArmR",
+                    localPos = new Vector3(-2f, 6f, 0f),
+                    localAngles = new Vector3(45f, -90f, 0f),
+                    localScale = generalScale * 10
+                }
+            });
+            rules.Add("mdlEngi", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ghostPrefab,
+                    childName = "Pelvis",
+                    localPos = new Vector3(-0.25f, 0f, 0f),
+                    localAngles = new Vector3(0f, -0.05f, 0f),
+                    localScale = generalScale
+                }
+            });
+            rules.Add("mdlMage", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ghostPrefab,
+                    childName = "Pelvis",
+                    localPos = new Vector3(-0.21f, 0f, 0f),
+                    localAngles = new Vector3(0f, -0.05f, 0f),
+                    localScale = generalScale
+                }
+            });
+            rules.Add("mdlMerc", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ghostPrefab,
+                    childName = "Pelvis",
+                    localPos = new Vector3(-0.22f, 0f, 0f),
+                    localAngles = new Vector3(0f, -0.05f, 0f),
+                    localScale = generalScale
+                }
+            });
+            rules.Add("mdlTreebot", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ghostPrefab,
+                    childName = "WeaponPlatform",
+                    localPos = new Vector3(0.2f, 0.05f, 0.2f),
+                    localAngles = new Vector3(-45f, 0f, 0f),
+                    localScale = generalScale * 2
+                }
+            });
+            rules.Add("mdlLoader", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ghostPrefab,
+                    childName = "Pelvis",
+                    localPos = new Vector3(-0.22f, 0f, 0f),
+                    localAngles = new Vector3(0f, -0.05f, 0f),
+                    localScale = generalScale
+                }
+            });
+            rules.Add("mdlCroco", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ghostPrefab,
+                    childName = "Hip",
+                    localPos = new Vector3(-2.2f, 0f, 0f),
+                    localAngles = new Vector3(-10f, -0.05f, 0f),
+                    localScale = generalScale * 10
+                }
+            });
+            rules.Add("mdlCaptain", new ItemDisplayRule[]
+{
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ghostPrefab,
+                    childName = "Pelvis",
+                    localPos = new Vector3(-0.22f, 0f, 0f),
+                    localAngles = new Vector3(0f, -0.05f, 0f),
+                    localScale = generalScale
+                }
+});
+        
+
+        CustomItem item = new CustomItem(def, rules);
+        GhostIndex = ItemAPI.Add(item);
         }
 
         private static void orig_CharacterBody_OnInventoryChanged(On.RoR2.CharacterBody.orig_OnInventoryChanged orig, CharacterBody self)
