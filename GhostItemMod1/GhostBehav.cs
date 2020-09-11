5a2d0debd4fdc6d7b455f3fe510a80d7ca500418
Paymon the Psyker <56231316+templarscoutb@users.noreply.github.com>
2020-09-10 23:17:14 -0400

    Extended

--- GhostItemMod1/GhostBehav.cs
+++ GhostItemMod1/GhostBehav.cs
@@ -12,49 +12,53 @@ namespace GhostItemMod1
     public class GhostBehav : MonoBehaviour
     {
 
-        [FormerlySerializedAs("teleporterActive")] public bool respawnActive = true;
-
+        public bool respawnActive = true;
+        public bool flag = false;
 
         public void Awake()
         {
 
             // Register all the hooks
             On.RoR2.HealthComponent.TakeDamage += orig_TakeDamage;
-            On.RoR2.TeleporterInteraction.OnInteractionBegin += orig_OnTPInteractionBegin;
-            On.RoR2.SceneDirector.PlaceTeleporter += orig_OnTPPlace;
+            On.RoR2.TeleporterInteraction.GetInteractability += orig_OnInteraction;
             On.RoR2.GoldshoresMissionController.OnEnable += orig_GSMC;
             On.RoR2.ArenaMissionController.OnEnable += orig_ARMC;
             On.RoR2.ArtifactTrialMissionController.Awake += orig_ARTMC;
             On.RoR2.MoonMissionController.OnEnable += orig_MMC;
         }
+        
 
         private void EnteringDarknessZone()
         {
             Chat.AddMessage("Guardian! You have entered a darkness zone!");
         }
 
-        private void orig_OnTPPlace(On.RoR2.SceneDirector.orig_PlaceTeleporter orig, SceneDirector self)
+        private Interactability orig_OnTPPlace(On.RoR2.SceneDirector.orig_PlaceTeleporter orig, SceneDirector self)
         {
-            // When the teleporter is placed on a level, set teleporter Active to false and call orig
-            respawnActive = true;
-            Chat.AddMessage("Guardian! You have exited the darkness zone.");
+            // When the teleporter is placed on a level check if flag is false. If false then it is a normal map and you can respawn outside of the teleporter event
+            if (flag == false)
+            {
+                respawnActive = true;
+                Chat.AddMessage("Guardian! You have exited the darkness zone.");
+            }
+            else if (flag == true)
+                respawnActive = false;
+            
             orig(self);
         }
-        
 
-        private void orig_OnTPInteractionBegin(On.RoR2.TeleporterInteraction.orig_OnInteractionBegin orig, TeleporterInteraction self, Interactor activator)
+
+        private void orig_OnInteraction(On.RoR2.TeleporterInteraction.orig_GetInteractability orig,
+            TeleporterInteraction self, Interactor activator)
         {
-            // When the teleporter is interacted with, set teleporter Active to true and call orig
-            respawnActive = false;
-            EnteringDarknessZone();
-            orig(self, activator);
-       
+            
         }
 
         private void orig_GSMC(On.RoR2.GoldshoresMissionController.orig_OnEnable orig, GoldshoresMissionController self)
         {
             //If in the Golden Shore Ghost cannot respawn you
             respawnActive = false;
+            flag = true;
             EnteringDarknessZone();
             orig(self);
          
@@ -64,6 +68,7 @@ namespace GhostItemMod1
         {
             //If in the Null Realm Ghost cannot respawn you
             respawnActive = false;
+            flag = true;
             EnteringDarknessZone();
             orig(self);
        
@@ -73,6 +78,7 @@ namespace GhostItemMod1
         {
             //If in the artificial realm Ghost cannot respawn you
             respawnActive = false;
+            flag = true;
             EnteringDarknessZone();
             orig(self);
    
@@ -82,6 +88,7 @@ namespace GhostItemMod1
         {
             //If on the Moon GHost cannot respawn you
             respawnActive = false;
+            flag = true;
             EnteringDarknessZone();
             orig(self);
 
